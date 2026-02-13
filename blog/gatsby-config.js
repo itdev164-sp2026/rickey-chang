a9629dfd54a6d515a10ab2ddd800a21c8f4e7165
Rickey Chang <changr1@gmatc.matc.edu>
2026-02-13 15:07:49 -0600

    Adding .env

--- blog/gatsby-config.js
+++ blog/gatsby-config.js
@@ -3,6 +3,9 @@
  *
  * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-config/
  */
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`
+})
 
 /**
  * @type {import('gatsby').GatsbyConfig}
@@ -23,8 +26,8 @@ module.exports = {
     {
       resolve: `gatsby-source-contentful`,
       options: {
-        spaceId: `n9lhcuzvq3wh`,
-        accessToken: `FXsGumXWJVy95qnovtwNkxmXvce4vpeLiVyqAQM8k9g`,
+        spaceId: `${process.env.SPACE_ID}`,
+        accessToken: `${process.env.ACCESS_TOKEN}`
       },
     },
     `gatsby-plugin-image`,
@@ -35,6 +38,7 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    `gatsby-transformer-remark`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
