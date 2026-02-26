458636aca1f6ae7d0828027148e69f9fcd6de423
Rickey Chang <changr1@gmatc.matc.edu>
2026-02-25 18:40:21 -0600

    Completed Activity 4

--- blog/src/components/header.js
+++ blog/src/components/header.js
@@ -8,10 +8,12 @@ padding: var(--space-4) var(--size-gutter);
 display: flex;
 align-items: center;
 justify-content: space-between;
+background: ${props => props.theme.header.backgroundColor}
 `
 const StyledLink = styled(Link)`
 font-size: var(--font-sm);
 text-decoration: none;
+color: ${props => props.theme.header.color}
 `
 
 const Header = ({ siteTitle }) => (
